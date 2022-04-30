c415bfd4621b53bd32ff36782db13a39bc80fced
Ahmed Edrees <ahmededrees30@gmail.com>
2022-04-30 05:54:31 +0200

    feat: add delete feature for each recipe

--- src/components/Recipe/RecipeList/RecipeList.jsx
+++ src/components/Recipe/RecipeList/RecipeList.jsx
@@ -1,55 +1,40 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import Recipe from "../../../apis/Recipe";
 import RecipeCard from "./RecipeCard/RecipeCard";
 
 export default function RecipeList() {
-  const recipes = [
-    {
-      _id: 1,
-      title: "potato recipe",
-      ingrediant:
-        "5 spones of lkns lkndsflkn lknsdlnf lsjdlfkj lksdjflk lkjdslkfj",
-      recipe:
-        "lkndsklfn lkdsflkjsd lkjdslkfj lksjdflkj lksdjflj lkjdsflkj lkjdflk",
-      image:
-        "https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&resize=768,574",
-    },
-    {
-      _id: 2,
-      title: "potato recipe",
-      ingrediant:
-        "5 spones of lkns lkndsflkn lknsdlnf lsjdlfkj lksdjflk lkjdslkfj",
-      recipe:
-        "lkndsklfn lkdsflkjsd lkjdslkfj lksjdflkj lksdjflj lkjdsflkj lkjdflk",
-      image:
-        "https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&resize=768,574",
-    },
-    {
-      _id: 3,
-      title: "potato recipe",
-      ingrediant:
-        "5 spones of lkns lkndsflkn lknsdlnf lsjdlfkj lksdjflk lkjdslkfj",
-      recipe:
-        "lkndsklfn lkdsflkjsd lkjdslkfj lksjdflkj lksdjflj lkjdsflkj lkjdflk",
-      image:
-        "https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&resize=768,574",
-    },
-  ];
+  const [recipes, setRecipes] = useState([]);
+
+  const handelDeleteRecipe = (id) => {
+    Recipe.delete("recipes/" + id).then(() => {
+      const updatedRecipes = recipes.filter((recipe) => recipe._id !== id);
+      setRecipes(updatedRecipes);
+    });
+  };
+
+  useEffect(() => {
+    Recipe.get("recipes").then((res) => setRecipes(res.data.data));
+  }, []);
+
   return (
     <div>
       <h1 className="text-6xl mb-8 text-yellow-400 font-bold font-mono">
         My Recipes
       </h1>
-      <div className="flex flex-col gap-4">
-        {recipes.map(({ _id, title, ingrediant, image }) => (
-          <RecipeCard
-            key={_id}
-            id={_id}
-            title={title}
-            ingrediant={ingrediant}
-            image={image}
-          />
-        ))}
-      </div>
+      {recipes.length ? (
+        <div className="flex flex-col gap-4">
+          {recipes.map(({ _id, title, ingredient, image }) => (
+            <RecipeCard
+              key={_id}
+              id={_id}
+              title={title}
+              ingredient={ingredient}
+              image={image}
+              handelDeleteRecipe={handelDeleteRecipe}
+            />
+          ))}
+        </div>
+      ) : null}
     </div>
   );
 }
