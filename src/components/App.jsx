ad881009c27873636c850eed80d4fe6919ad1c43
Ahmed Edrees <ahmededrees30@gmail.com>
2022-04-30 06:09:04 +0200

    feat: add routing

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,17 @@
+import { Routes, Route } from "react-router-dom";
 import "./App.css";
+import AddRecipe from "./Recipe/AddRecipe/AddRecipe";
+import RecipeDetails from "./Recipe/RecipeDetails/RecipeDetails";
 import RecipeList from "./Recipe/RecipeList/RecipeList";
 
 function App() {
   return (
     <div className="App p-10 container  mx-auto">
-      <RecipeList />
+      <Routes>
+        <Route path="/" element={<RecipeList />} />
+        <Route path="/add-recipe" element={<AddRecipe />} />
+        <Route path="/recipe/:id" element={<RecipeDetails />} />
+      </Routes>
     </div>
   );
 }
